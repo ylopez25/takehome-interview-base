b714c6fa8b97ec2b82abd2348edb6dffb9e23526
Yesenia L <ylopezcr123@gmail.com>
2022-08-21 21:09:31 -0400

    expand/collapse LD

--- src/Menu.js
+++ src/Menu.js
@@ -1,13 +1,25 @@
 import { data } from './data';
+import {useState} from 'react';
 import './Menu.css';
 import MenuItem from './MenuItem';
 
 const Menu = () => {
+const [open, setOpen] = useState([]);
+
+const showLongDes = (id) => {
+  //if open which is an empty arr includes id , filter id that is not equal to el
+  if(open.includes(id)) {
+    setOpen(open.filter(el => el !== id))
+  }else {
+    //add id to arr
+    setOpen([...open, id])
+  }
+}
   return (
     <article className="menu">
       <h1>Our Menu</h1>
       {data.map((item) => (
-        <MenuItem key={item.id} item={item} />
+        <MenuItem key={item.id} item={item} onClick={() => showLongDes(item.id)} open={open.includes(item.id)}/>
       ))}
     </article>
   );
--- src/MenuItem.js
+++ src/MenuItem.js
@@ -1,7 +1,7 @@
 import './MenuItem.css';
 
-const MenuItem = ({ item }) => {
-  const { id, name, price, shortDescription } = item;
+const MenuItem = ({ item, onClick, open }) => {
+  const { id, name, price, shortDescription, longDescription} = item;
   return (
     <section className="menuItem">
       <ul>
@@ -9,6 +9,10 @@ const MenuItem = ({ item }) => {
         <li>name: {name} </li>
         <li>price: ${price} </li>
         <li>{shortDescription}</li>
+        <button onClick={onClick}>
+          <i>{open ? "collapse" : "expand"}</i>
+        </button>
+        <li>{open ? longDescription : null}</li>
       </ul>
     </section>
   );
